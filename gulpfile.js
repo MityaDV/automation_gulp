ba10e43626ba4c87254ea859e7e2aece0d0a18a8
Dmitry Kovalkov <46884389+MityaDV@users.noreply.github.com>
2019-10-16 15:41:37 +0300

    настроил task css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 "use strict";
 
 var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
 
 gulp.task("css", function() {
   return gulp.src("source/sass/style.scss")
-  .pipe(gulp.dest("source/css"));
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"));
 })
