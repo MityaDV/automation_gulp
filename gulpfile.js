59227ca6e8783f53a4b835d1d1e49c2db009f0ca
Dmitry Kovalkov <46884389+MityaDV@users.noreply.github.com>
2019-10-16 16:46:01 +0300

    настроил task оптимизации изображений

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 
 gulp.task("css", function() {
   return gulp.src("source/sass/style.scss")
@@ -21,4 +22,15 @@ gulp.task("css", function() {
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"));
+});
+
+gulp.task("images", function () {
+  return guip.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+
+    .pipe(gulp.dest("source/img"));
 })
