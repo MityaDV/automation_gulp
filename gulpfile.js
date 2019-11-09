65d758368097842d8f530f9ab63169b0768c93ff
Dmitry Kovalkov <46884389+MityaDV@users.noreply.github.com>
2019-11-09 22:38:32 +0200

    добавил слежение за папкой img

--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/img/*.{png,jpg,svg}", gulp.series("images", "webp", "refresh"));
   gulp.watch("source/img/sprite-svg-icons/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
